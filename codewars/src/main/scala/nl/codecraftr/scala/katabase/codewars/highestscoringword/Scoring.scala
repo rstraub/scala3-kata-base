b7abf123072a9afc8ee918a3d47de8a537c5f67e
Roy Straub <roy@codecraftr.nl>
2024-05-21 21:35:05 +0200

    Find highest scoring words

--- codewars/src/main/scala/nl/codecraftr/scala/katabase/codewars/highestscoringword/Scoring.scala
+++ codewars/src/main/scala/nl/codecraftr/scala/katabase/codewars/highestscoringword/Scoring.scala
@@ -4,7 +4,8 @@ object Scoring {
   private def scores =
     ('a' to 'z').zipWithIndex.map { case (c, i) => (c, i + 1) }.toMap
 
-  def high(str: String): String = ""
+  def high(str: String): String =
+    str.split(' ').map(w => score(w) -> w).maxBy(_._1)._2
 
   def score(word: String): Int = word.map(scores(_)).sum
 }
